f4b1405968f620ef43e5dde6d7ad84185603968e
handod <69374221+handod@users.noreply.github.com>
2021-02-06 20:42:25 +0200

    2d commit

--- src/components/my-navbar/navbarComponent.jsx
+++ src/components/my-navbar/navbarComponent.jsx
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import styled from 'styled-components';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Logo from '../../assets/icons/logow.jpg';
 import './navbarStyle.css';
 
+const Transition = styled.div`
+  .active {
+    visibility: visible;
+    transition: all 200ms ease-in;
+  }
+  .hidden {
+    visibility: hidden;
+    transition: all 200ms ease-out;
+    transform: translate(0, -100%);
+  }
+`;
 
 const NavbarComponent = () => {
+
+  const [showNavbar, setShowNavbar] = useState(true)
+  const [scrollPos, setScrollPos] = useState(0)
+
+  const handleScroll  =() =>{
+    setScrollPos(document.body.getBoundingClientRect().top)
+    setShowNavbar(document.body.getBoundingClientRect().top > scrollPos)
+    console.log(` navDisplay > ${navDisplay} || showNavBar ${showNavbar} || scrollPos ${scrollPos}`)
+
+  };
+
+  let navDisplay = showNavbar? "active": "hidden"
+  useEffect(()=>{
+    window.addEventListener("scroll", handleScroll);
+    return ()=>{
+      window.removeEventListener("scroll", handleScroll);
+    }   
+  });
+
     return (
-        <>
-        <Navbar fixed="top" collapseOnSelect expand="md" variant="dark" className="animate-navbar nav-theme justify-content-between">
+        <Transition>
+        <Navbar fixed="top" collapseOnSelect expand="md" variant="dark" className={`nav-theme justify-content-between ${navDisplay}`}>
   <Navbar.Brand href="#home">
       <img className="logo" src={Logo} alt='Saidon Portfolio Logo' />
   </Navbar.Brand>
@@ -25,7 +56,7 @@ const NavbarComponent = () => {
     </Nav>
   </Navbar.Collapse>
 </Navbar>
-        </>
+        </Transition>
     )
 }
 
